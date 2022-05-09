bcb224eee20c019a595a2bcb4f73898738689794
hieu <hquang@unm.edu>
2022-05-08 18:17:10 -0600

    V9
    Added basic instructions in the main

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 int main () {
+	// INPUTS: Vector {}, starting state, "00"
 	states x({1,0,0,0,1,1,1,0,1},"00");
 	x.print();
 	
+	// INPUTS: bit string (MUST BE EVEN and 0's and 1's ONLY) 
 	viterbi y("000101110010");
 	y.print();
 	return 0;
